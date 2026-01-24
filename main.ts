d3480593c541a904ceee7bcca031532f7c2bdc3d
Dr John Vidler <j.vidler@lancaster.ac.uk>
2026-01-24 18:25:30 +0000

    Migrated from the core lib, added dependency on the latest core

--- main.ts
+++ main.ts
@@ -1,1 +1,28 @@
+//% block="Rib:Bit Storage"
+//% color="#c47122"
+//% icon="\u26A0"
+//% groups=[ "Files", "Data" ]
+namespace RibBitSD {
+    //% block="switch microSD card $state \u26A0"
+    export function switchSD(state: RibBit.OnOff = RibBit.OnOff.On): void {
+        return
+    }
 
+    //% block="create a new log called $name \u26A0"
+    //% group="Files"
+    export function setLogName(name: string = "newlog"): void {
+        return;
+    }
+
+    //% block="uSD card present \u26A0"
+    //% advanced="true"
+    export function isSDPresent(): boolean {
+        return false;
+    }
+
+    //% block="log data $data \u26A0"
+    //% group="Data"
+    export function logData(data: any): void {
+        return;
+    }
+}
